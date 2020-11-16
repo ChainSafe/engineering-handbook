d2a6f3863a5421e917e18aa4ef709fadeba114a9
Gregory Markou <gregorymarkou@gmail.com>
2020-11-16 13:50:13 -0500

    update remote

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -5,7 +5,7 @@ WIKI_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
 git remote add wiki https://$GITHUB_API_KEY@github.com/ChainSafe/engineering-wiki.wiki.git > /dev/null 2>&1
 git fetch wiki
-git checkout wiki master
+git checkout wiki wiki/master
 git merge origin/main --no-edit
 git push wiki HEAD:master > /dev/null 2>&1
 
